aeb5afd92de2ddfd5b0f51207fc2f5e41103363c
selimnill <mrrafi812345@gmail.com>
2024-01-02 22:34:32 +0600

    initial commit

--- src/Components/VideoDetails.jsx
+++ src/Components/VideoDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactPlayer from "react-player/youtube";
 import { BsFillCheckCircleFill } from 'react-icons/bs';
@@ -13,11 +13,13 @@ const VideoDetails = () => {
 
   const [video, setVideo] = useState();
   const [relatedVideos, setRelatedVideos] = useState();
-  const { id } = useParams;
+  const { id } = useParams();
   const { setLoading } = useContext(Context);
 
   useEffect(() => {
-    fetchDataFromApi();
+    document.getElementById("root").classList.add("custom-h"); 
+    fetchVideoDetails();
+    fetchRelatedVideos();
   }, [id]);
 
   const fetchVideoDetails = () => {
@@ -27,10 +29,19 @@ const VideoDetails = () => {
       setVideo(res);
       setLoading(false);
     })
+  };
+
+  const fetchRelatedVideos = () => {
+    setLoading(true);
+    fetchDataFromApi(`video/related-contents/?id=${id}`).then(res => {
+      console.log(res);
+      setVideo(res);
+      setLoading(false);
+    })
   }
 
   return (
-    <div>
+    <div className='flex justify-center flex-row h-[calc('>
       rs
     </div>
   )
