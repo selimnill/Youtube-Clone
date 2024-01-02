9d668f013867d3ead4d757b21d0c999230d0356a
selimnill <mrrafi812345@gmail.com>
2024-01-02 22:22:45 +0600

    video details implement

--- src/Components/VideoDetails.jsx
+++ src/Components/VideoDetails.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ReactPlayer from "react-player/youtube";
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { AiOutlineLike } from 'react-icons/ai';
+import { abbreviateNumber } from 'js-abbreviation-number';
+import { fetchDataFromApi } from "../utils/api";
+import SuggestionVideoCard from "./SuggestionVideoCard";
+import { Context } from '../Context/ContextApi';
+
 
 const VideoDetails = () => {
+
+  const [video, setVideo] = useState();
+  const [relatedVideos, setRelatedVideos] = useState();
+  const { id } = useParams;
+  const { setLoading } = useContext(Context);
+
+  useEffect(() => {
+    fetchDataFromApi();
+  }, [id]);
+
+  const fetchVideoDetails = () => {
+    setLoading(true);
+    fetchDataFromApi(`video/details/?id=${id}`).then(res => {
+      console.log(res);
+      setVideo(res);
+      setLoading(false);
+    })
+  }
+
   return (
-    <div>VideoDetails</div>
+    <div>
+      rs
+    </div>
   )
 }
 
